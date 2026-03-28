13920b76b2f5f1a0833e6367d43e7c1a196e2336
Supreet <supreetkumar32@gmail.com>
2026-03-28 19:06:36 +0530

    initial commit

---
+++ src/test/java/com/fifteen/ecommerce/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifteen.ecommerce.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
